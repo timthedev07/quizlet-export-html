2086b9b175b230bfa707f74f08c2cd555a383616
Tim <harvard90873@gmail.com>
2023-04-08 22:51:24 +0200

    clean up

--- web/src/components/layout.tsx
+++ web/src/components/layout.tsx
@@ -3,21 +3,6 @@ import { DefaultSeo } from "next-seo";
 import defaultConfig from "../lib/seo-config";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  // Uncomment this part to switch between desktop & mobile nav
-  // const [windowSize, setWindowSize] = useState<number>(0);
-
-  // useEffect(() => {
-  //   setWindowSize(window.innerWidth);
-  //   const resizeHandler = () => {
-  //     setWindowSize(window.innerWidth);
-  //   };
-  //   window.addEventListener("resize", resizeHandler);
-
-  //   return () => {
-  //     window.removeEventListener("resize", resizeHandler);
-  //   };
-  // }, []);
-
   return (
     <>
       <DefaultSeo {...defaultConfig} />
