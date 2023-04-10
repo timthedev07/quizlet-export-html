c51888a0eb6c96db1bec7a90de665e98c19e2d23
Tim <harvard90873@gmail.com>
2023-04-10 17:41:20 +0200

    next auth stuff

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { DragontailProvider } from "dragontail-experimental";
+import { SessionProvider } from "next-auth/react";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <DragontailProvider theme="dark">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </DragontailProvider>
+    <SessionProvider session={session}>
+      <DragontailProvider theme="dark">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </DragontailProvider>
+    </SessionProvider>
   );
 };
 
